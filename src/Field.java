17deff20ec3a0cfa82918a8bee240749c6942b34
Stepanyuk Eugeniya <_zh_box@mail.ru>
2013-08-09 11:58:46 +0800

    zh.gbox@gmail.com

--- src/Field.java
+++ src/Field.java
@@ -2,17 +2,22 @@ public class Field {
 
     public static final int FIELD_SIZE = 3;
 
-    private char [][] cell = new char [FIELD_SIZE][FIELD_SIZE];
+    private char [][] cell = new char [FIELD_SIZE][FIELD_SIZE];//можно сделать private
 
-    public boolean cross = true;
+    public boolean cross = true; //лучше бы сделать это поле private, добавить getter и setter
 
     private void clearLine(int line){
         for(int i = 0; i < FIELD_SIZE; i++){
             cell[line][i] = ' ';
+            //сделать тут  константу типа static final char DEFAULT_CELL_VALUE = ' '  ??
         }
     }
 
     private void showLine(int line){
+        //в этом месте мы знаем количество строк в строке,, можем сдеать цикл
+        //это будет удобнее когда будем менять размерность поля
+        //for(int i = 0; i < FIELD_SIZE; i++)
+        //    System.out.println(cell[i][line]);
         System.out.println(cell[0][line] + " " + cell[1][line] + " " + cell[2][line]);
     }
 
@@ -22,6 +27,9 @@ public class Field {
         }
     }
 
+    //возможно вместо флага  cross лучше передавать в функию параметр char 'X' или char 'O'
+    // можно будет избавиться от лишней переменной и вызывать функий можно будет field.getFieldCell(x,y, 'X')
+    //функцию тогда надо будет объявить  public void setFieldCell(int x, int y, char symbol)
     public void setFieldCell(int x, int y){
         if (x >= 0 && x < FIELD_SIZE && y >= 0 && y < FIELD_SIZE){
             if (cross) {
@@ -31,8 +39,10 @@ public class Field {
                 cell[x][y] = 'O';
             }
         }
-        cross = !cross;
+        cross = !cross; //тут определяется крестик сейчас ходит или нолик верно?
     }
+
+
 
     public char getFieldCell(int x, int y) {
         if (x >= 0 && x < FIELD_SIZE && y >= 0 && y < FIELD_SIZE){
